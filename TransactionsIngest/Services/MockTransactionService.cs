da39857e29edcc0b37223c17839e15f86ba96490
jgunti1 <jgunti1@gmail.com>
2026-03-12 15:19:32 -0500

    Verify upsert, update detection, and revocation working correctly

--- TransactionsIngest/Services/MockTransactionService.cs
+++ TransactionsIngest/Services/MockTransactionService.cs
@@ -17,8 +17,8 @@ public class MockTransactionService : ITransactionService
         var transactions = new List<TransactionDto>
         // hardcoded values
         {
-             new(1001, "4111111111111111", "STO-01", "Wireless Mouse", 19.99m, DateTime.UtcNow.AddHours(-2)),
-            new(1002, "4000000000000002", "STO-02", "USB-C Cable", 25.00m, DateTime.UtcNow.AddHours(-5)),
+             new(1001, "4111111111111111", "STO-01", "Wireless Mouse", 24.99m, DateTime.UtcNow.AddHours(-2)),
+            //new(1002, "4000000000000002", "STO-02", "USB-C Cable", 25.00m, DateTime.UtcNow.AddHours(-5)),
             new(1003, "5500000000000004", "STO-01", "HDMI Cable", 15.49m, DateTime.UtcNow.AddHours(-1)),
             new(1004, "4111111111111111", "STO-03", "Keyboard", 45.00m, DateTime.UtcNow.AddHours(-10))
 
