544a3ca4cec716e3378ed7adb0182c6f4c459650
jgunti1 <jgunti1@gmail.com>
2026-03-12 13:33:12 -0500

    Add Progam.cs with dependency injection and app wiring

--- TransactionsIngest/Program.cs
+++ TransactionsIngest/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TransactionsIngest.Data;
+using TransactionsIngest.Services;
+
+var configuration = 
+    new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json",
+                    optional: false).Build();
+
+var services = new ServiceCollection();
+
+//Register database
+
+services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+
+//Register services
+var useMockFeed = configuration.GetValue<bool>("AppSettings:UseMockFeed");
+
+if (useMockFeed)
+    services.AddScoped<ITransactionService, MockTransactionService>();
+
+services.AddScoped<IngestionService>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+//Run database migrations
+using (var scope = serviceProvider.CreateScope())
+{
+    var db = scope.serviceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
+}
+
+// Run ingestion
+using (var scope = serviceProvider.CreateScope())
+{
+    var ingestionService = scope.ServiceProvider.GetRequiredService<ingestionService>();
+    await ingestionService.RunAsync();
+}
